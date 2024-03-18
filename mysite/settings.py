7ceb0cff2e88ddca884efb3ff49770bc242ed6e7
Ajeet KumarKushwaha <ajitkwebdeveloper@gmail.com>
2024-03-18 11:49:01 +0530

    proxy setting added

--- mysite/settings.py
+++ mysite/settings.py
@@ -143,3 +143,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000/'
+]
